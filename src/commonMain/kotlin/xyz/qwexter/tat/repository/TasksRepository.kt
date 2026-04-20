8e08f807dea548d9bd1c81fa62b695412edec8d5
Aleksandr Bubnov <69006636+qwexter@users.noreply.github.com>
2026-04-20 17:48:40 +0400

    Make a simple contract for a tasks repository and mock for it "get all method"

---
+++ src/commonMain/kotlin/xyz/qwexter/tat/repository/TasksRepository.kt
@@ -0,0 +1,36 @@
+package xyz.qwexter.tat.repository
+
+import kotlinx.datetime.LocalDateTime
+import xyz.qwexter.tat.models.Task
+import xyz.qwexter.tat.models.TaskPriority
+import xyz.qwexter.tat.models.TaskStatus
+
+interface TasksRepository {
+    suspend fun allActiveTasks(): List<Task>
+    suspend fun createTask(
+        name: String,
+        description: String?,
+        status: TaskStatus = TaskStatus.Todo,
+        priority: TaskPriority = TaskPriority.Low,
+        deadline: LocalDateTime?,
+    )
+
+    companion object Mock : TasksRepository {
+
+        private val allTasks = mutableListOf<Task>()
+
+        override suspend fun allActiveTasks(): List<Task> {
+            return allTasks.filter { it.deletedAt == null }.toList()
+        }
+
+        override suspend fun createTask(
+            name: String,
+            description: String?,
+            status: TaskStatus,
+            priority: TaskPriority,
+            deadline: LocalDateTime?,
+        ) {
+            TODO("Not yet implemented")
+        }
+    }
+}
