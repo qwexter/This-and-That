f35147788c89865b9bc98b3c84e3453bd05bcbcb
Aleksandr Bubnov <69006636+qwexter@users.noreply.github.com>
2026-05-09 15:32:12 +0400

    Add logging and update docs

--- src/commonMain/kotlin/xyz/qwexter/Routing.kt
+++ src/commonMain/kotlin/xyz/qwexter/Routing.kt
@@ -4,9 +4,9 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.BadRequestException
+import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.request.httpMethod
 import io.ktor.server.request.uri
-import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
